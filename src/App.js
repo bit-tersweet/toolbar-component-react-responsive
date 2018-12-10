5845a918de687e2566f3debb04485e152cfa536d
Giuseppe Gentile <giuseppe.gentile@issgreppi.it>
2018-12-10 09:20:43 +0100

    Added swipe gesture for mobile browsers

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import MyNav from './Components/navbar';
 import Drawer from './MenuOpener/Drawer';
 import Backdrop from './Backdrop/Backdrop';
+import Swipeable from 'react-swipeable';
 
 
 class App extends React.Component {
@@ -17,22 +18,32 @@ class App extends React.Component {
 
 
     backdropClickHandler = () => {
-        this.setState({sideDrawerOpen:false});
+        this.setState({ sideDrawerOpen: false });
+    };
+
+    a = () => {
+        this.setState({ sideDrawerOpen: true });
     };
 
     render() {
         let backDrop;
 
         if (this.state.sideDrawerOpen) {
-            backDrop = <Backdrop click={this.backdropClickHandler}/>;
+            backDrop = <Backdrop click={this.backdropClickHandler} />;
         }
 
         return (
-            <div style={{ height: '100%' }}>
-                <MyNav drawerClickHandler={this.drawerToggleClickHandler}/>
-                <Drawer show={this.state.sideDrawerOpen}/>
-                {backDrop}
-            </div>
+            <Swipeable onSwipingLeft={this.backdropClickHandler} >
+                <Swipeable onSwipingRight={this.a}>
+                    <div className="myDiv" style={{ height: '100%' }}>
+
+                        <MyNav drawerClickHandler={this.drawerToggleClickHandler} />
+                        <Drawer show={this.state.sideDrawerOpen} />
+                        {backDrop}
+
+                    </div>
+                </Swipeable>
+            </Swipeable>
         );
     }
 }
--- src/MenuOpener/ToggleButton.js
+++ src/MenuOpener/ToggleButton.js
@@ -3,11 +3,10 @@ import '../Stylesheets/ToggleStyle.css';
 
 const ToggleButton = props => (
     <button className="toggle-button" onClick={props.click}>
-        <div className="toggle-button__line"/>
-        <div className="toggle-button__line"/>
-        <div className="toggle-button__line"/>
+        <div className="toggle-button__line" />
+        <div className="toggle-button__line" />
+        <div className="toggle-button__line" />
     </button>
-
 );
 
 export default ToggleButton;
